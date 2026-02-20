c6c6a8bd29db6d68dc2f674e11cf25a235e6e881
Pratham Jaiswal <100998543+pantha704@users.noreply.github.com>
2026-02-20 22:53:15 +0530

    Delete persistent-todo-queue directory

--- persistent-todo-queue/src/todo.rs
+++
@@ -1,8 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-
-#[derive(Debug, BorshSerialize, BorshDeserialize)]
-pub struct Todo {
-    pub id: u64,
-    pub description: String,
-    pub created_at: u64,
-}
